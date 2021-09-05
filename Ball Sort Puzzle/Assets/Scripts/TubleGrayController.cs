06f25c31301917b2a06ae69bea129833dce0f6a1
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-09-05 21:51:04 +0700

    add tube_gray

---
+++ Ball Sort Puzzle/Assets/Scripts/TubleGrayController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TubleGrayController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject TubleGray;
+    public Transform target;
+    void Start()
+    {
+        int x = 0;
+        for(int i=0;i<7 ;i++ )
+        {
+           Instantiate(TubleGray, target.position, Quaternion.identity);
+            Vector3 test = new Vector3(1, 2, 3);
+            target.position = test;
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
