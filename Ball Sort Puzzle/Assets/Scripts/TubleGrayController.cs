80f91d4a51757184cf2bf1ad51cb529e349bd52e
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-09-07 23:37:14 +0700

    ramdom TubleGray

--- Ball Sort Puzzle/Assets/Scripts/TubleGrayController.cs
+++ Ball Sort Puzzle/Assets/Scripts/TubleGrayController.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class TubleGrayController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static TubleGrayController Intansce;
     public GameObject TubleGray;
-    public Transform target;
+    GameObject[] AllTubleGray = new GameObject[7];
+
+    // int[] a;
     void Start()
+       // 3.35
     {
-        int x = 0;
-        for(int i=0;i<7 ;i++ )
+        Intansce = this;
+        Vector3 Poschange;
+        float x = 0;
+       
+        for (int i=0;i<7 ;i++ )
         {
-           Instantiate(TubleGray, target.position, Quaternion.identity);
-            Vector3 test = new Vector3(1, 2, 3);
-            target.position = test;
-        }    
+
+            AllTubleGray[i]= Instantiate(TubleGray, transform.position, Quaternion.identity);
+            x = 3.35f;
+
+            Poschange.x = transform.position.x + x;
+            Poschange.y = transform.position.y;
+            Poschange.z = 0;
+            transform.position = Poschange;
+
+            if (i == 4)
+            {
+                Poschange.x = -7.3f;
+                Poschange.y = -5.65f;
+                Poschange.z = 0f;
+                transform.position = Poschange;
+            }
+           
+
+        }
     }
 
     // Update is called once per frame
