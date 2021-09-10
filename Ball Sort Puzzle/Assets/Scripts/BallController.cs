84d69e057193c7c2b56594533dc1e17edbc45652
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-09-10 21:45:08 +0700

    click object

--- Ball Sort Puzzle/Assets/Scripts/BallController.cs
+++ Ball Sort Puzzle/Assets/Scripts/BallController.cs
@@ -40,31 +40,38 @@ public class BallController : MonoBehaviour
     {
         //    Stack stackball = new Stack();
         //  GameObject[] stackclone = new GameObject[4];
-        Vector3 target = TubleGrayController.Intansce.arrayList[0].transform.position;
+        Vector3 target;// = TubleGrayController.Intansce.arrayList[0].transform.position;
 
         float y = 0;
-        for (int i = 0; i < 4; i++)
+        for (int a=0;a<5 ;a++ )
         {
-            int a = Random.RandomRange(0, 5);
-           
-            Vector3 change = new Vector3();
-            change.x = target.x;
-            change.y = target.y + y;
-            change.z = target.z;
-            target = change;
+            target = TubleGrayController.Intansce.arrayList[a].transform.position;
+            target.y = target.y - 1.65f;
+            y = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                int b = Random.RandomRange(0, 5);
 
-            /*   GameObject obj = Instantiate(AllBall[a], transform.position, Quaternion.identity);        */      /// bug cái này sinh ra tại bên trong cái bình cơ
-            GameObject obj = Instantiate(AllBall[a],target, Quaternion.identity);
-            Myball.Add(obj);
-         //   stackclone[i] = obj;
-            y =1.05f;
-          //  stackball.Push(obj);
+                Vector3 change = new Vector3();
+                change.x = target.x;
+                change.y = target.y + y;
+                change.z = target.z;
+                target = change;
+
+                /*   GameObject obj = Instantiate(AllBall[a], transform.position, Quaternion.identity);        */      /// bug cái này sinh ra tại bên trong cái bình cơ
+                GameObject obj = Instantiate(AllBall[b], target, Quaternion.identity);
+                Myball.Add(obj);
+                //   stackclone[i] = obj;
+                y = 1.05f;
+                //  stackball.Push(obj);
+            }
+            /*     GameObject de =*/ //stackball.Pop();
+
+            //   Destroy(stackclone[2]);
+            //  Destroy(Myball[0]);
         }
-        /*     GameObject de =*/ //stackball.Pop();
 
-        //   Destroy(stackclone[2]);
-    //  Destroy(Myball[0]);
-    }   
+    }
     //    public void setupball()
     //{
     ////    Stack stackball = new Stack();
---
+++ Ball Sort Puzzle/Assets/Scripts/ObjectMoveController.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectMoveController : MonoBehaviour
+{
+
+    public float force = 5;
+
+    private void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        // //   Debug.LogError("da tim thay ");
+
+        //    RaycastHit hit;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //    if (Physics.Raycast(ray, out hit, 100.0f))
+        //    {
+        //        if (hit.transform != null)
+        //        {
+        //            Debug.LogError("da tim thay ");
+
+        //            //Rigidbody rb;
+
+        //            //if (rb = hit.transform.GetComponent<Rigidbody>())
+        //            //{
+        //            //    PrintName(hit.transform.gameObject);
+        //            //    LaunchIntoAir(rb);
+        //            //}
+        ////        }
+        ////    }
+        //}
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            //    hit.collider.attachedRigidbody.AddForce(Vector2.up);
+            }
+        }
+    }
+
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
+
+    private void LaunchIntoAir(Rigidbody rig)
+    {
+        rig.AddForce(rig.transform.up * force, ForceMode.Impulse);
+    }
+
+}
