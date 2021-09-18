34e3b75694e0832020a246643dd6d724dddb14a0
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-09-18 09:48:51 +0700

    move ball, randomball true

--- Ball Sort Puzzle/Assets/Scripts/Controller.cs
+++ Ball Sort Puzzle/Assets/Scripts/Controller.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using System;
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
     public static Controller Instance;
-    public GameObject Tuble;
-    GameObject[] Allball=new GameObject[10];
-    public int[] random = new int[5];
-    void Start()
-    {
 
+    public GameObject Tuble;
+
+    internal GameObject[] Allball = new GameObject[10];
+
+    public int[] random = new int[5];
+
+    internal void Start()
+    {
     }
+
     public void taotube()
     {
 
@@ -25,7 +26,7 @@ public class Controller : MonoBehaviour
         {
             GameObject NTuble = Instantiate(Tuble, transform.position, Quaternion.identity);
             NTuble.tag = i.ToString();
-            NTuble.GetComponent<Tube>().Create();
+            NTuble.GetComponent<Tube>().Create(i, i + 1);
             Allball[i] = NTuble;
 
             x = 3.35f;
@@ -43,17 +44,48 @@ public class Controller : MonoBehaviour
                 transform.position = Poschange;
             }
         }
-
-      
-      
     }
+
+    public void testrandom()
+    {
+        for (int i = 0; i < random.Length; i++)
+        {
+            Debug.LogError("ball thu" + i + "da xuat hieen" + random[i] + "lan");
+        }
+    }
+
     public void xoatube()
     {
         Allball[1].GetComponent<Tube>().xoa();
     }
 
-    void Update()
+    internal void Update()
     {
-      
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                int tubeselect = Int32.Parse(hit.collider.gameObject.tag);
+                Debug.LogError("tubleselect is" + tubeselect);
+                MoveBall(tubeselect, 2);  
+              
+            }
+
+        }
     }
+    void MoveBall(int ps1,int ps2)
+    {
+        Allball[ps1].GetComponent<Tube>().balls.Peek().color.transform.position = Allball[ps2].GetComponent<Tube>().balls.Peek().color.transform.position;
+
+
+        Allball[ps2].GetComponent<Tube>().setBall(Allball[ps1].GetComponent<Tube>().getBall());
+    }
+    
 }
+
+
+
--- Ball Sort Puzzle/Assets/Scripts/Tube.cs
+++ Ball Sort Puzzle/Assets/Scripts/Tube.cs
@@ -10,7 +10,7 @@ public class Tube : MonoBehaviour
     {
     }
 
-    public void Create()
+    public void Create(int deball, int nextdeball)
     {
         //balls = new Stack<Ball>(4);
         //for (int i = 0; i < 4; i++)
@@ -21,54 +21,81 @@ public class Tube : MonoBehaviour
         //    a.color = objectball;
         //    balls.Push(a);
         //}
+      
         float z = 1.37f;
         balls = new Stack<Ball>(4);
         float y = 0;
         Vector3 tagert = transform.position;
-        int rando = 1;
 
-        int b=0;
-        for (int i = 0; i < 4; i++)
+
+        if (deball!=5&& nextdeball!=6&&nextdeball!=7)
         {
-            Ball a = new Ball();
-            while (rando == 1)
+            for (int i = 0; i < 4; i++)
             {
-                b = Random.RandomRange(0, 5);  //0-4
-                Debug.LogError("_______________random______" + b);
-              
-                Debug.LogError( Controller.Instance.random[b]);
-                Controller.Instance.random[b]++;
-                if (Controller.Instance.random[b] < 4)
-                {
-                    rando = 0;
-                }
-               
+                Ball a = new Ball();
+                int b = random();
+                Vector3 change = new Vector3();
+
+                change.x = tagert.x;
+                change.y = tagert.y + y - z;
+                change.z = tagert.z;
+                tagert = change;
+
+                GameObject objectball = Instantiate(allBall[b], tagert, Quaternion.identity);
+
+                //  Debug.LogError("romdom mau bong" + b);
+                a.color = objectball;
+
+                balls.Push(a);
+                y = 1.05f;
+                z = 0;
+
             }
-            Vector3 change = new Vector3();
-
-            change.x = tagert.x;
-            change.y = tagert.y + y - z;
-            change.z = tagert.z;
-            tagert = change;
-
-            GameObject objectball = Instantiate(allBall[b], tagert, Quaternion.identity);
-            rando = 1;
-          //  Debug.LogError("romdom mau bong" + b);
-            a.color = objectball;
-
-            balls.Push(a);
-            y = 1.05f;
-            z = 0;
-
         }
     }
-
+    int random()
+    {
+        int b=-1;
+        int run = 1;
+        while (run==1)
+        {
+            b = Random.Range(0, 5);
+            Controller.Instance.random[b]++;
+            if(Controller.Instance.random[b]<=4)
+            {
+                run = 0;
+            }
+        }
+        return b;
+    }
     public void xoa()
     {
-        Destroy(balls.Pop().color);
+        // Destroy(balls.Pop().color);
+      
+    }
+    public Ball getBall()
+    {
+        Vector3 change = new Vector3();
+        change.x = balls.Pop().color.transform.position.x;
+        change.y = balls.Pop().color.transform.position.y + 2f;
+        change.z = balls.Pop().color.transform.position.z;
+        balls.Pop().color.transform.position = change;
+        return balls.Pop();
+    }
+    public void setBall(Ball newball)
+    {
+        balls.Push(newball);
     }
 
+    public void DeleteAllBall()
+    {
+       while(balls.Count!=0)
+        {
+            Destroy(balls.Pop().color);
+        }
+    }
     internal void Update()
     {
+       
     }
 }
